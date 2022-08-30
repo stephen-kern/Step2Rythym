b8588bd5171dc0f878dbdc5ffc8e2216395c1ce2
White Buffalo <apothocare@gmail.com>
2022-08-30 13:30:57 -0500

    Basic java for genius api pull

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,149 +1,52 @@
-
 let searchInputEl = document.querySelector("#search-bar");
 //User text entry for Artist
-let artistInput = document.getElementById('artistInput');
+let artistInput = document.getElementById("searchArtist");
 //User text entry for Song name
 let songInput = document.getElementById('songInput');
-//Server that site will be hosted on
-let redirect_uri = "http://127.0.0.1:5500/index.html";
-//Spotify auth numbers
-let client_id = "63d3486515dc43269b27ab2aa50b79f2";
-let client_secret = "313a1d1dbd4149069b2c051fb49e2e1a";
-// Authorize url
-const authorize = "https://accounts.spotify.com/authorize";
-//Token req Url
-const TOKEN = "https://accounts.spotify.com/api/token";
 
-function onPageLoad(){
-    if (window.location.search.length > 0){
-        handleRedirect();
-        
-    }
-};
+//let client_id = 'RZW6cI7SahSMcoIvDUIzRqkb4LAhMSW4wDBfxbDsXS1CfTnLJTUZcWa1AlHD03Wp'
+//let client_secret = 'jhWoIv7gioCmc_dEnUxtv0nJAELUdEsYj3xFv1uH_WFVTiHrcCopr3yD1tmedOcItu123BSyuXXsxB1TAml7aA'
+let apiToken= 'K1i8ef3ZhUTwUfB_noiHI4Q6K6NjQXNx8oE8uKYp9iOiutPCcFPrvEm81n3ixg9h'
 
-function handleRedirect(){
-    let code = getCode();
-    fetchAccessToken(code);
-    window.history.pushState(",", redirect_uri);
+//User info submit
+function handleSubmit(event){
+    event.preventDefault();
+    const artistInput = document.getElementById("searchArtist").value;
+    const songInput = document.getElementById('songInput').value;
+    console.log(songInput);
+    fetchSongs(songInput);
 }
 
-function fetchAccessToken(code){
-    let body = "grant_type=authorization_code";
-    body += "&code=" + code;
-    body += "&redirect_uri=" + encodeURI(redirect_uri);
-    body += "&client_id=" + client_id;
-    body += "&client_secret=" + client_secret;
-    callAuthorizationApi(body);
-}
+//get lyrics from Gnius
+function fetchSongs (songInput) {
+    // format the github api url and replace spaces with %20
+    const cleanSongInput = songInput.replace(/\s/g, '%20');
+    //var apiUrl = "https://api.genius.com/songs/${id}?=${ACCESS_TOKEN}"
+    var apiUrl = `https://api.genius.com/search?q=${cleanSongInput}&access_token=K1i8ef3ZhUTwUfB_noiHI4Q6K6NjQXNx8oE8uKYp9iOiutPCcFPrvEm81n3ixg9h`;
+    // make a get request to url
+    fetch(apiUrl)
+      .then((response) => {
+        return response.json();})
+        //this is where we target what we want
+        .then((data) =>{
+            console.log(data.response.hits[0].result.artist_names);
+        })}
 
-function callAuthorizationApi(body){
+        //error handle request code
+    //     // request was successful
+    //     if (response.ok) {
+    //       console.log(response);
+    //       response.json().then(function(data) {
+    //         console.log(data);
+    //         getSongLyrics(data, song);
+    //       });
+    //     } else {
+    //       alert('Error: Song Lyrics Not Found');
+    //     }
+    //   })
+    //   .catch(function(error) {
+    //     alert("Unable to connect to Search Engine");
+    //   })
+    //     console.log(this)};
 
-    let xhr = new XMLHttpRequest();
-     xhr.open("POST", TOKEN, true);
-     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-     xhr.setRequestHeader('Authorization', 'Basic' + btoa(client_id + ":" + client_secret) );
-     xhr.send(body);
-     xhr.onload = handleAuthorizationResponse;
-    // const APIController = (function(){
-    //     const getToken = async () =>{
-    //         const result = await fetch(TOKEN, {
-    //             method: 'POST',
-    //          headers: {
-    //                 'Content-Type': 'application/x-www-form-urlencoded',
-    //                 'Authorization': 'Basic' + btoa(client_id + ":" + client_secret)
-    //             },
-    //             body: 'grant_type=client_credentials'
-    //         });
-    //         const data = await result.json();
-    //         return data.access_token;
-    //     }});
-    
-}
-
-function handleAuthorizationResponse(){
-    if(this.status = 200){
-        var data = JSON.parse(this.responseText);
-        console.log(data);
-        var data = JSON.parse(this.responseText);
-        if (data.access_token != undefined){
-            access_token = data.access_token;
-            localStorage.setItem("access_token", access_token);
-        }
-        if(data.refresh_token !=undefined){
-            refresh_token = data.refresh_token;
-            localStorage.setItem("refresh_token", refresh_token);
-        }
-        onPageLoad();
-    }else{
-        console.log(this.responseText);
-        alert(this.responseText);
-    }
-
-}
-//
-    function getCode(){
-        let code = null;
-         
-        const queryString = window.location.search;
-        if (queryString.length > 0){
-         const urlParams = new URLSearchParams(queryString);
-            code = urlParams.get('code')
-        }
-            return code;
-    }
-   
-
-//request authorization from spotify
-function requestAuthorization(){
-    let url = authorize;
-    url += "?client_id=" + client_id;
-    url += "&response_type=code";
-    url += "&redirect_uri=" + encodeURI(redirect_uri);
-    url += "&show_dialog=true";
-    url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played user-top-read playlist-modify-public"
-    window.location.href = url; //Show Spotify's authorization screen
-}
-
-
-
-//function to accept both the array of response data and the term we searches as parameters
-// var displaySongs = function(songs, searchTerm) {
-
-//     //check if api returned any repos
-//     if (songs.length === 0) {
-//         songContainerEl.textContent = "No songs found.";
-//         return;
-//     }
-//     console.log (songs);
-//     console.log(searchTerm);
-
-//     //clear old content
-//     songContainerEl.textContent= "";
-//     songSearchTerm.textContent= searchTerm;
-
-//     //loop over repos
-//     for (var i=0; i< songs.length; i++){
-//         //format song name
-//         var songName=repos[i].owner.login + "/" + repos[i].name;
-
-//         //create containers for each repo
-//         var songEl = document.createElement("div");
-//         songEl.classlist = "list-item flex-row justify-space-between align-center";
-
-//         //create span element to hold repo name
-//         var titleEl=document.createElement("span");
-//         titleEl.textContent = songName;
-
-//         //append to container
-//         songEl.appendChild(titleEl);
-
-//         //append container to the dom
-//         songContainerEl.appendChild(songEl);
-//     }
-// };
-// function btnCheck(event){
-//     event.preventDefault();
-// console.log(artistInput.value);
-// console.log(songInput.value);
-// }
-// searchInputEl.addEventListener("submit", getlyrics)
+      searchInputEl.addEventListener("submit", handleSubmit)
