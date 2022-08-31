6b835ee9b5649e41d19d1ab4bee91abf1580075a
stephen <94320530+stephen-kern@users.noreply.github.com>
2022-08-31 12:24:02 -0500

    trying to figure out append

--- assets/java/news.js
+++ assets/java/news.js
@@ -1,4 +1,4 @@
-let apiToken = "07858fbf6a24ae5c4a99e163daf462d0";
+let newsToken = "07858fbf6a24ae5c4a99e163daf462d0";
 let searchWord = document.getElementById("searchArtist");
 const articlesContainerEl = document.querySelector("#articlesBox");
 
@@ -10,27 +10,28 @@ const formSubmitHandler = function (event) {
   console.log(search);
   if (search) {
     getArticles(search);
+    displayArticleCard(search);
     searchWord.value = "";
   } else {
-    alert("Please enter a valid Country");
+    alert("Please enter an Artist");
   }
 };
 
 // wrap api fetch inside a function
-const getArticles = function (userSearch) {
-  let search = userSearch.replace(/\s/g, "+");
+const getArticles = function (data) {
+  let search = searchWord.replace(/\s/g, "+");
 
   // format the github api url
-  var apiUrl = `https://gnews.io/api/v4/search?q=${search}&token=${apiToken}`;
+  var apiUrl = `https://gnews.io/api/v4/search?q=${search}&token=${newsToken}`;
 
   // make a request to the url
   fetch(apiUrl)
     .then(function (response) {
       // request was successful
       if (response.ok) {
-        response.json().then(function (data) {
-          console.log(data);
-          renderArticleData(search);
+        response.json().then(function (search) {
+          console.log(search);
+          displayArticleCard(search);
         });
       } else {
         alert("Error: Articles Not Found");
@@ -42,25 +43,49 @@ const getArticles = function (userSearch) {
     });
 };
 
-// append the articles here 
-const renderArticleData = (search) => {
-  const articlesContainerEl = $("#articlesBox");
-  const articleCard = $("<div>")
-    .attr("id", "article-card")
-    .addClass("card p-4 m-4 w-40 is-two-thirds");
-  const articleTitle = search.title;
-  const articleDesc = $("<p>").text(search.description).addClass("is-size-3");
-  // const articleSrc = $("<h4>").text(search.source.name);
-  // const articleUrl = search.source.url;
+// // append the articles here 
+// const renderArticleData = (search) => {
+//   const articlesContainerEl = $("#articlesBox");
+//   const articleCard = $("<div>")
+//     .attr("id", "article-card")
+//     .addClass("card p-4 m-4 w-40 is-two-thirds");
+//   const articleTitle = search.title;
+//   const articleDesc = $("<p>").text(search.description).addClass("is-size-3");
+//   // const articleSrc = $("<h4>").text(search.source.name);
+//   // const articleUrl = search.source.url;
 
-  articleCard.append(
-    articleTitle,
-    articleDesc,
-    // articleSrc
-  );
+//   articleCard.append(
+//     articleTitle,
+//     articleDesc,
+//     // articleSrc
+//   );
   
-  articlesContainerEl.append(articleCard);
-};
+//   articlesContainerEl.append(articleCard);
+// };
+function displayArticleCard(userSearch) {
+  let hits = search.response.hits.length;
+    for (let i = 0; i < hits; i++){
+        relatedArticlessArr.push(data.response.hits[i].result)
+    };
+
+  let articlesContainer = document.getElementById('article-card-container');
+
+  const articleCard = document.createElement('div');
+  articleCard.classList.add('song-card', "column", "is-12","is-mobile");
+
+  const articleTitle = document.createElement('h3');
+  articleTitle.classList.add('song-title');
+  songTitle.innerText = relatedArticlesArr[i].title;//put array obj data here
+
+  const articleSrc = document.createElement('h4');
+  articleSrc.classList.add('song-artist');
+  articleSrc.innerText = relatedArticlesArr[i].source.name.url;//put array obj data here
+
+  articlesContainer.appendChild(articleCard);
+  articleCard.appendChild(articleTitle);
+  articleCard.appendChild(songArtist);
+  articleCard.appendChild(albumArt);
+}
 
 $("#search-form").on("submit", formSubmitHandler);
 
