13edf2e9401883df9c7d3a768ec9cb2845b9d4d7
White Buffalo <apothocare@gmail.com>
2022-08-31 01:14:02 -0500

    Added search bar to single-page. Added CoverArt img when song is searched

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,8 +1,8 @@
-let searchInputEl = document.querySelector("#search-bar");
+let searchInputEl = document.querySelector("#search-form");
 //User text entry for Artist
 let artistInput = document.getElementById("searchArtist");
 //User text entry for Song name
-let songInput = document.getElementById('songInput');
+let songInput = document.getElementById('searchSong');
 
 //let client_id = 'RZW6cI7SahSMcoIvDUIzRqkb4LAhMSW4wDBfxbDsXS1CfTnLJTUZcWa1AlHD03Wp'
 //let client_secret = 'jhWoIv7gioCmc_dEnUxtv0nJAELUdEsYj3xFv1uH_WFVTiHrcCopr3yD1tmedOcItu123BSyuXXsxB1TAml7aA'
@@ -12,14 +12,13 @@ let url = window.location.href;
 function handleSubmit(event){
     event.preventDefault();
     const artistInput = document.getElementById("searchArtist").value;
-    const songInput = document.getElementById('songInput').value;
-    console.log(songInput);
-    fetchSongs(songInput);
-    redirect();
+    const songInput = document.getElementById('searchSong').value;
+    fetchSongInfo(songInput);
+   // redirect();
 }
 
 //get lyrics from Gnius
-function fetchSongs (songInput) {
+function fetchSongInfo (songInput) {
     // format the github api url and replace spaces with %20
     const cleanSongInput = songInput.replace(/\s/g, '%20');
     //var apiUrl = "https://api.genius.com/songs/${id}?=${ACCESS_TOKEN}"
@@ -29,30 +28,39 @@ function fetchSongs (songInput) {
       .then((response) => {
         return response.json();})
         //this is where we target what we want
-        .then((data) =>{
-            console.log(data.response.hits[0].result.artist_names);
-        })}
+        .then((data) =>{//Recieved JSOn OBJ with all song data
+
+            //setting that cover art url to a var
+         let coverArt = (data.response.hits[0].result.header_image_url);//Cover art image
+            //adding cover art to single-page
+            function addArt(){
+                document.getElementById("cover_art").src = coverArt
+                
+                
+
+            }
+            addArt();
+
+
+
+
+       
+        //    console.log(data.response.hits[0].result.artist_names);//artist name
+        //    console.log(data.response.hits[0].result.path);//lyrics url path
+           console.log(data);//whole json data obj
+
+        
+        })
+    
+
+    }
+    
 
 function redirect(){
     //replace this with url + '/single-page'
     window.location.href = 'http://127.0.0.1:5500/single-page.html';
 }
 
-function getArtistImg(songInput){
-        // format the github api url and replace spaces with %20
-        const cleanSongInput = songInput.replace(/\s/g, '%20');
-        //var apiUrl = "https://api.genius.com/songs/${id}?=${ACCESS_TOKEN}"
-        var apiUrl = `https://api.genius.com/search?q=${cleanSongInput}&access_token=K1i8ef3ZhUTwUfB_noiHI4Q6K6NjQXNx8oE8uKYp9iOiutPCcFPrvEm81n3ixg9h`;
-        // make a get request to url
-        fetch(apiUrl)
-          .then((response) => {
-            return response.json();})
-            //this is where we target what we want
-            .then((data) =>{
-                let updateArtistImgurl = (data.response.hits[0].result.header_image_url);
-                document.getElementById('artistImg').src = updateArtistImgurl
-                
-            })}
 
 
 
