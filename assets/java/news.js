dbd5e4e500729b59e9916f562363a3619dd89988
stephen <94320530+stephen-kern@users.noreply.github.com>
2022-08-31 14:40:15 -0500

    append first five new articles to page

--- assets/java/news.js
+++ assets/java/news.js
@@ -1,6 +1,5 @@
 let newsToken = "07858fbf6a24ae5c4a99e163daf462d0";
 let searchWord = document.getElementById("searchArtist");
-const articlesContainerEl = document.querySelector("#articlesBox");
 
 // wrap search input inside a eventlistener
 const formSubmitHandler = function (event) {
@@ -10,7 +9,6 @@ const formSubmitHandler = function (event) {
   console.log(search);
   if (search) {
     getArticles(search);
-    displayArticleCard(search);
     searchWord.value = "";
   } else {
     alert("Please enter an Artist");
@@ -18,8 +16,8 @@ const formSubmitHandler = function (event) {
 };
 
 // wrap api fetch inside a function
-const getArticles = function (data) {
-  let search = userSearch.replace(/\s/g, "+");
+const getArticles = function (searchWord) {
+  let search = searchWord.replace(/\s/g, "+");
 
   // format the github api url
   var apiUrl = `https://gnews.io/api/v4/search?q=${search}&token=${newsToken}`;
@@ -29,9 +27,11 @@ const getArticles = function (data) {
     .then(function (response) {
       // request was successful
       if (response.ok) {
-        response.json().then(function (search) {
-          console.log(search);
-          displayArticleCard(search);
+        return response.json().then(function (data) {
+          console.log(data.articles.slice(0, 5));
+          let fiveArticles = data.articles.slice(0, 5);
+          fiveArticles.forEach((article) => displayArticleCard(article));
+          // displayArticleCard(data);
         });
       } else {
         alert("Error: Articles Not Found");
@@ -43,49 +43,43 @@ const getArticles = function (data) {
     });
 };
 
-// // append the articles here 
-// const renderArticleData = (search) => {
-//   const articlesContainerEl = $("#articlesBox");
-//   const articleCard = $("<div>")
-//     .attr("id", "article-card")
-//     .addClass("card p-4 m-4 w-40 is-two-thirds");
-//   const articleTitle = search.title;
-//   const articleDesc = $("<p>").text(search.description).addClass("is-size-3");
-//   // const articleSrc = $("<h4>").text(search.source.name);
-//   // const articleUrl = search.source.url;
+function displayArticleCard(article) {
+  console.log(article);
+  console.log(article.title);
 
-//   articleCard.append(
-//     articleTitle,
-//     articleDesc,
-//     // articleSrc
-//   );
+  let articlesContainer = document.getElementById("article-card-container");
+
+  const articleCard = document.createElement("div");
+  articleCard.classList.add("column", "is-12", "is-mobile");
+
+  const articleTitle = document.createElement("h3");
+  articleTitle.classList.add("h3");
+  articleTitle.innerText = article.title;
+
+  const articleDesc = document.createElement("p");
+  articleDesc.classList.add("p");
+  articleDesc.innerText = article.description;
+
+  const articleSrcName = document.createElement('h4');
+  articleSrcName.classList.add('h4');
+  articleSrcName.innerText = article.source.name;
+
+  const articleSrcUrl = document.createElement('a');
+  articleSrcUrl.classList.add('a');
+  articleSrcUrl.href = article.source.url;
+  articleSrcUrl.innerText = article.source.name;
+
+
+
+  articleCard.append(
+    articleTitle,
+    articleDesc,
+    articleSrcName,
+    articleSrcUrl
+    );
   
-//   articlesContainerEl.append(articleCard);
-// };
-function displayArticleCard(userSearch) {
-  let hits = search.response.hits.length;
-    for (let i = 0; i < hits; i++){
-        relatedArticlessArr.push(data.response.hits[i].result)
-    };
-
-  let articlesContainer = document.getElementById('article-card-container');
-
-  const articleCard = document.createElement('div');
-  articleCard.classList.add('song-card', "column", "is-12","is-mobile");
-
-  const articleTitle = document.createElement('h3');
-  articleTitle.classList.add('song-title');
-  songTitle.innerText = relatedArticlesArr[i].title;//put array obj data here
-
-  const articleSrc = document.createElement('h4');
-  articleSrc.classList.add('song-artist');
-  articleSrc.innerText = relatedArticlesArr[i].source.name.url;//put array obj data here
-
-  articlesContainer.appendChild(articleCard);
-  articleCard.appendChild(articleTitle);
-  articleCard.appendChild(songArtist);
-  articleCard.appendChild(albumArt);
+  articlesContainer.append(articleCard);
+  
 }
 
 $("#search-form").on("submit", formSubmitHandler);
-
