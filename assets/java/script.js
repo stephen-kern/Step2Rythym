5bfc00d9c3f1c9a959685bf70ac704adfb05aab1
White Buffalo <apothocare@gmail.com>
2022-08-31 17:10:57 -0500

    song cards now work as links to artist

--- assets/java/script.js
+++ assets/java/script.js
@@ -60,7 +60,10 @@ function fetchSongInfo (songInput) {
     
                 const albumArt = document.createElement('img');
                 albumArt.setAttribute('src', relatedArtistsArr[i].header_image_thumbnail_url);
-
+                albumArt.onclick = function(){
+                    let songInput = relatedArtistsArr[i].artist_names;
+                   fetchSongInfo(songInput);
+                }
                 const songTitle = document.createElement('h3');
                 songTitle.classList.add('song-title');
                 songTitle.innerText = relatedArtistsArr[i].title;//put array obj data here
